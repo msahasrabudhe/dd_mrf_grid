a2cffc85b9e9aa4fc439ac5e19bc6f267dbcdfa4
msahasrabudhe <mihir.s.sahasrabudhe@gmail.com>
2017-03-23 09:34:57 +0100

    Renamed to test_general

---
+++ test_general.py
@@ -0,0 +1,24 @@
+import dd_mrf_grid as ddmrf
+import matplotlib.pyplot as plt
+
+np = ddmrf.np
+
+n_labels = 2
+grid_size = [50, 50]
+lat = ddmrf.Lattice(grid_size[0], grid_size[1],n_labels)
+
+for i in range(lat.n_nodes):
+	ne	= np.random.standard_normal(size=(n_labels,))
+	lat.set_node_energies(i, ne)
+
+for e in lat._find_empty_attributes()[1]:
+	ne	= np.random.standard_normal(size=(n_labels, n_labels))
+	ne	= 2*ne			# Make sigma = 1
+	lat.set_edge_energies(e[0], e[1], ne)
+
+if __name__ == '__main__':
+	lat.optimise(a_start=0.1, max_iter=5000)
+	print np.reshape(lat.labels, grid_size)
+	plt.plot(lat.dual_costs, 'r-')
+	plt.show()
+
