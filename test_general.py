d94074f08ce9fea6c986d6eb37d3a42844a6ef96
msahasrabudhe <mihir.s.sahasrabudhe@gmail.com>
2019-09-03 17:39:34 +0200

    ms: Updated testing method.

--- test_general.py
+++ test_general.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 np = ddmrf.np
 
 n_labels = 2
-grid_size = [50, 50]
+grid_size = [20, 20]
 lat = ddmrf.Lattice(grid_size[0], grid_size[1],n_labels)
 
 for i in range(lat.n_nodes):
@@ -13,12 +13,12 @@ for i in range(lat.n_nodes):
 
 for e in lat._find_empty_attributes()[1]:
 	ne	= np.random.standard_normal(size=(n_labels, n_labels))
-	ne	= 2*ne			# Make sigma = 1
+	ne	= 2*ne			# Make sigma = 2
 	lat.set_edge_energies(e[0], e[1], ne)
 
 if __name__ == '__main__':
 	lat.optimise(a_start=0.1, max_iter=5000)
 	print np.reshape(lat.labels, grid_size)
-	plt.plot(lat.dual_costs, 'r-')
+	lat.plot_costs()
 	plt.show()
 
